786cedfc445f365005cc7e74292a0330ac79845b
abhiram1504 <abhiramnellutla1504@gmail.com>
2020-09-30 22:12:00 +0530

    mod

--- routes/app.js
+++ routes/app.js
@@ -2,34 +2,34 @@ const express = require('express');
 const { db } = require('../models/Hospital');
 const router = express.Router();
 const Hospital = require('../models/Hospital');
-const Ventilator = require('../models/Ventilators');
+const Ventilator = require('../models/Ventilator');
 
 //Reading the  Hospital details
 router.get('/getH',(req,res,next)=>{
-    db.collection("hospitals").find({}).toArray( function(err, outcome) {
+    db.collection("Hospital").find({}).toArray( function(err, outcome) {
         if (err) throw err;
         res.send(outcome);
         console.log(outcome);
     });
 });
 
-//Reading the Ventilators  details 
+//Reading the Ventilator  details 
 router.get('/getV',(req,res,next)=>{
-    db.collection("ventilators").find({}).toArray( function(err, outcome) {
+    db.collection("Ventilator").find({}).toArray( function(err, outcome) {
         if (err) throw err;
         res.send(outcome);
         console.log(outcome);
     });
 });
 
-//Adding new hospitals
+//Adding new Hospital
 router.post('/addH',(req,res,next)=>{
     Hospital.create(req.body).then(function(hospital){
           res.send(hospital);
       }).catch(next);
 });
 
-//Adding new ventilators
+//Adding new Ventilator
 router.post('/addV',(req,res,next)=>{
     Ventilator.create(req.body).then(function(ventilator){
             res.send(ventilator);
@@ -38,28 +38,29 @@ router.post('/addV',(req,res,next)=>{
 
 //Updating ventilator details
 router.put('/update/:id',(req,res,next)=>{
-   Ventilator.updateOne({ventilatorId: req.params.id},req.body).then((ventilator)=>{
+   Ventilator.updateOne({vId: req.params.id},req.body).then((ventilator)=>{
+       console.log(req.params.id)
        res.send({ventilator});
     }).catch(next);
 });
 
 //Searching hospital by name
 router.get('/get/:name',(req,res,next)=>{
-    db.collection("hospitals").findOne({name: req.params.name}).then(function(hospital){
+    db.collection("Hospital").findOne({name: req.params.name}).then(function(hospital){
         res.send(hospital);
     });
 });
 
-//Searching ventilators by status and hospital name
+//Searching Ventilator by status and hospital name
 router.get('/get/:name/:status',(req,res,next)=>{
-     db.collection("ventilators").find({name: req.params.name,status: req.params.status}).toArray().then(function(ventilator){
+     db.collection("Ventilator").find({name: req.params.name,status: req.params.status}).toArray().then(function(ventilator){
           res.send(ventilator);
       });
 });
 
 //Deleting Ventilator by Vent ID
 router.delete('/delete/:id',(req,res,next)=>{
-    db.collection("ventilators").deleteOne({ventilatorId: req.params.id}).then(function(ventilator){
+    db.collection("Ventilator").deleteOne({vId: req.params.id}).then(function(ventilator){
         res.send(ventilator);
     });
 });
